1ff90b2d748e9ebb36ce4e5cc3ae785b0dee733a
Tornelius Broadwater jr <tkbjr01@icloud.com>
2024-03-05 16:32:05 -0500

    Added input handler to keep track of keys selected

--- game-components/game.js
+++ game-components/game.js
@@ -1,10 +1,12 @@
 import Player from "./player.js"
+import InputHandler from "./input.js";
 
 export default class Game {
   constructor(width, height) {
     this.width = width
     this.height = height
     this.player = new Player(this)
+    this.input = new InputHandler();
   }
 
   update() {
