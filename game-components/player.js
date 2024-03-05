af4b469e823628baffdbfffd360cb1a2cc089dc8
Tornelius Broadwater jr <tkbjr01@icloud.com>
2024-03-05 16:08:02 -0500

    created player class

---
+++ game-components/player.js
@@ -0,0 +1,17 @@
+export default class Player {
+  constructor(game) {
+    this.game = game;
+    this.width = 100;
+    this.height = 90;
+    this.x = 0;
+    this.y = 100;
+  }
+
+  update() {
+
+  }
+
+  draw(context) {
+    context.fillRect(this.x, this.y, this.width, this.height);
+  }
+}
